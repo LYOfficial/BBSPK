e398259788e3a5fa75d5c866703d465bb63819a0
sheep-realms <sheep_realms@qq.com>
2024-03-09 04:47:43 +0800

    fix

--- res/script/common.js
+++ res/script/common.js
@@ -4,6 +4,7 @@ $('#forum-count').text(db_forums.length);
 $('#forum-count-ex').text(db_forums_ex.length);
 
 $(document).on('click', '.forum-item-header', function(e) {
+    console.log(e);
     if (e.target.nodeName == "A") return;
     let $sel = $(this).parents('.forum-item').find('.forum-item-content');
     if ($sel.hasClass('active')) {
