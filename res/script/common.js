d77d0be3e8e97e5450ab061ee19dad6ac8e7543b
sheep-realms <sheep_realms@qq.com>
2024-03-09 20:33:56 +0800

    优化 header

--- res/script/common.js
+++ res/script/common.js
@@ -1,6 +1,13 @@
 $('#forum-list').html(DOMForumList.list(db_forums, 'forum-list'));
 $('#forum-list-ex').html(DOMForumList.listEx(db_forums_ex, 'forum-list-ex'));
 
+function scrollToTitle(id) {
+    if (id == 'top') return $(document).scrollTop(0);
+    const top = $('#' + id).offset().top;
+    const navHeight = $('#bbspk-nav').height();
+    $(document).scrollTop(top - navHeight);
+}
+
 $(document).on('click', '.forum-item-header', function(e) {
     if ($(e.target).closest('a').length) return;
     let $sel = $(this).parents('.forum-item').find('.forum-item-content');
@@ -17,4 +24,18 @@ $(document).on('click', '.list-controller-item.oepn', function() {
 
 $(document).on('click', '.list-controller-item.close', function() {
     $('#' + $(this).data('target') + ' .forum-item-content').removeClass('active');
+});
+
+$(document).on('click', 'nav .nav-item', function() {
+    const target = $(this).data('target');
+    scrollToTitle(target);
+});
+
+$(document).scroll(function() {
+    let top = $(document).scrollTop();
+    if (top > $('header').offset().top + $('header').height()) {
+        $('#bbspk-nav').addClass('in-sticky');
+    } else {
+        $('#bbspk-nav').removeClass('in-sticky');
+    }
 });
