2e53a529dbc676435c3726079bf9f17abf32878b
xiaocheng145 <150434323+xiaocheng145@users.noreply.github.com>
2024-03-17 15:31:18 +0800

    新增晋级论坛
    新增NWBBS

--- res/data/forums.js
+++ res/data/forums.js
@@ -637,7 +637,18 @@ const db_forums = [
         hasNetSec: "no",
         note: "",
         reference: []
-    }
+    },
+    {
+    title: "NWBBS",
+    url: "https://nwbbs.cn/",
+    state: "up",
+    createdAt: "2024/03/16",
+    updatedAt: "2024/03/17",
+    hasICP: "no",
+    hasNetSec: "no",
+    note: "(暂无)",
+    reference: []
+}
 ];
 
 
