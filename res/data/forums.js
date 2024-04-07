8cd2b07bc68fb6bf2df3c3c6b94a79c7b8c2a6e0
XiaoMi_WecYun <111235937+vwgw@users.noreply.github.com>
2024-04-07 22:25:25 +0800

    forums.js

--- res/data/forums.js
+++ res/data/forums.js
@@ -896,7 +896,18 @@ const db_forums = [
         hasNetSec: "no",
         note: "非大陆服务器。",
         reference: []
-    }
+    },
+    {
+        title: "MineMC我的世界中文资源论坛",
+        url: "https://mc422.zyz",
+        state: "up",
+        createdAt: "2023/10/19",
+        updatedAt: "2024/04/05",
+        hasICP: "no",
+        hasNetSec: "no",
+        note: "非大陆服务器。",
+        reference: []
+}
 ];
 
 
